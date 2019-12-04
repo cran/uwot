078789cca3f168f7c20c2cc88c9b038da1a64093
James Melville <jlmelville@gmail.com>
2019-12-04 15:20:02 +0000

    version 0.1.5

--- src/tauprng.h
+++ src/tauprng.h
@@ -22,46 +22,47 @@
 #ifndef UWOT_TAUPRNG_H
 #define UWOT_TAUPRNG_H
 
-#include <limits>
 #include "Rcpp.h"
+#include <limits>
 // linked from dqrng
+#include "convert_seed.h"
 #include "pcg_random.hpp"
 
+// NOT THREAD SAFE
 // based on code in the dqsample package
 static uint64_t random64() {
-  return R::runif(0, 1) * std::numeric_limits<uint64_t>::max();
+  return R::runif(0, 1) * (std::numeric_limits<uint64_t>::max)();
 }
 
-class tau_prng {
+// NOT THREAD SAFE
+static uint32_t random32() {
+  return R::runif(0, 1) * (std::numeric_limits<uint32_t>::max)();
+}
+
+struct tau_prng {
   uint64_t state0;
   uint64_t state1; // technically this needs to always be > 7
   uint64_t state2; // and this should be > 15
-  
+
   static constexpr uint64_t MAGIC0 = static_cast<uint64_t>(4294967294);
   static constexpr uint64_t MAGIC1 = static_cast<uint64_t>(4294967288);
   static constexpr uint64_t MAGIC2 = static_cast<uint64_t>(4294967280);
-  
-public:
-  tau_prng() {
-    state0 = random64();
-    state1 = random64();
-    state2 = random64();
-  }
-  
+
   tau_prng(uint64_t state0, uint64_t state1, uint64_t state2)
-    : state0(state0), state1(state1), state2(state2) {}
+      : state0(state0), state1(state1 > 7 ? state1 : 8),
+        state2(state2 > 15 ? state2 : 16) {}
 
   int32_t operator()() {
     state0 = (((state0 & MAGIC0) << 12) & 0xffffffff) ^
-      ((((state0 << 13) & 0xffffffff) ^ state0) >> 19);
+             ((((state0 << 13) & 0xffffffff) ^ state0) >> 19);
     state1 = (((state1 & MAGIC1) << 4) & 0xffffffff) ^
-      ((((state1 << 2) & 0xffffffff) ^ state1) >> 25);
+             ((((state1 << 2) & 0xffffffff) ^ state1) >> 25);
     state2 = (((state2 & MAGIC2) << 17) & 0xffffffff) ^
-      ((((state2 << 3) & 0xffffffff) ^ state2) >> 11);
+             ((((state2 << 3) & 0xffffffff) ^ state2) >> 11);
 
     return state0 ^ state1 ^ state2;
   }
-  
+
   // return a value in (0, n]
   std::size_t operator()(const std::size_t n) {
     std::size_t result = (*this)() % n;
@@ -69,14 +70,24 @@ public:
   }
 };
 
-class pcg_prng {
+struct tau_factory {
+  uint64_t seed1;
+  uint64_t seed2;
+  tau_factory() : seed1(random64()), seed2(random64()) {}
+
+  void reseed() {
+    seed1 = random64();
+    seed2 = random64();
+  }
+
+  tau_prng create(uint64_t seed) { return tau_prng(seed1, seed2, seed); }
+};
+
+struct pcg_prng {
 
   pcg32 gen;
-  
-public:
-  pcg_prng() { 
-    gen.seed(random64());
-  }
+
+  pcg_prng(uint64_t seed) { gen.seed(seed); }
 
   // return a value in (0, n]
   std::size_t operator()(const std::size_t n) {
@@ -85,6 +96,16 @@ public:
   }
 };
 
+struct pcg_factory {
+  uint32_t seed1;
+  pcg_factory() : seed1(random32()) {}
 
+  void reseed() { seed1 = random32(); }
+
+  pcg_prng create(uint32_t seed) {
+    uint32_t seeds[2] = {seed1, seed};
+    return pcg_prng(dqrng::convert_seed<uint64_t>(seeds, 2));
+  }
+};
 
 #endif // UWOT_TAUPRNG_H
